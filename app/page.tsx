bb579ff900280a8ec6fc5cc5edef9230a40bcdf1
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2024-02-13 16:47:42 -0500

    moved guests to useContext

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,54 @@
-'use client'
+"use client";
+import { useState } from "react";
+import { createContext } from "react";
 import Form from "./form";
 import Logo from "./logo";
 
+export type Guest = {
+  firstName: string;
+  lastName: string;
+  gender: "male" | "female";
+  age: number;
+  amountDue: number;
+  RSVP:'yes'|'no'|'maybe'|'pending'
+};
+export type GlobalContent = {
+  guests: Array<Guest> | null;
+  setGuests: React.Dispatch<React.SetStateAction<Guest[]>> | null;
+};
+
+export const GuestsContext = createContext<GlobalContent>({
+  guests: null,
+  setGuests: () => {},
+});
+
 export default function Home() {
+  const [guests, setGuests] = useState<Array<Guest>>([
+    {
+      firstName: "Person",
+      lastName: "1",
+      gender: "female",
+      age: 27,
+      amountDue: 100,
+      RSVP:'pending'
+    },
+    {
+      firstName: "Person",
+      lastName: "2",
+      gender: "male",
+      age: 23,
+      amountDue: 0,
+      RSVP:'pending'
+    },
+  ]);
+  const contextValue: GlobalContent = {
+    guests,
+    setGuests,
+  };
   return (
-    <main>
+    <GuestsContext.Provider value={contextValue}>
       <Logo/>
       <Form/>
-    </main>
+    </GuestsContext.Provider>
   );
 }
