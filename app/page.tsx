938cf43e3cdcf586efb7fac7199d0e80c65d8251
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2024-02-13 15:30:06 -0500

    adding home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+'use client'
+import Form from "./form";
+import Logo from "./logo";
 
 export default function Home() {
   return (
-    <main >
-<h1>TravelHub</h1>
+    <main>
+      <Logo/>
+      <Form/>
     </main>
   );
 }
