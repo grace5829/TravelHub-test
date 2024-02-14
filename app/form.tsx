77923e170bb755c244ae1eed916d74dc1b14ce95
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2024-02-14 12:18:46 -0500

    adding css to form

--- app/form.tsx
+++ app/form.tsx
@@ -2,19 +2,24 @@ import { ChangeEvent, FormEvent, useContext, useState } from "react";
 import { Guest, GuestsContext } from "./page";
 import styled from "styled-components";
 
-
 const TableWrapper = styled.span`
-margin:10px;
-display:flex;
-justify-content:center;
-flex-wrap:wrap;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
- const EachGuest = styled.div`
-display:grid;
-grid-template-columns: 200px 100px 100px 100px 100px;
+const EachGuest = styled.div`
+  display: grid;
+  grid-template-columns: 200px 100px 100px 100px 100px;
 `;
- const EachGuestInfo = styled.span`
-border:2px solid blue;
+const EachGuestInfo = styled.span`
+  border: 2px solid blue;
+`;
+const FormWrapper = styled.span`
+  display: grid;
+  grid-template-columns: 170px 170px 170px 170px;
+  row-gap: 10px;
+  padding: 10px;
 `;
 
 export default function Form() {
@@ -44,83 +49,88 @@ export default function Form() {
 
   return (
     <div>
-        <TableWrapper>
-
+      <form onSubmit={handleSubmit}>
+        <FormWrapper>
+          <label htmlFor="firstName">
+            First Name:
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={newGuest.firstName}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="lastName">
+            Last Name:
+            <input
+              type="text"
+              id="firstName"
+              name="lastName"
+              value={newGuest.lastName}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="age">
+            Age:
+            <input
+              type="number"
+              id="age"
+              name="age"
+              value={newGuest.age}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="amountDue">
+            Amount Due:
+            <input
+              type="number"
+              id="amountDue"
+              name="amountDue"
+              value={newGuest.amountDue}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="gender">
+            Gender:
+            <select id="gender" name="gender" onChange={handleChange}>
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+            </select>
+          </label>
+          <label htmlFor="RSVP">
+            RSVP:
+            <select id="RSVP" name="RSVP" onChange={handleChange}>
+              <option value="pending">Pending</option>
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+              <option value="maybe">Maybe</option>
+            </select>
+          </label>
+        </FormWrapper>
+        <input type="submit" />
+      </form>
+      <TableWrapper>
         <EachGuest>
-        <EachGuestInfo>Name</EachGuestInfo>
-        <EachGuestInfo>RSVP</EachGuestInfo>
-        <EachGuestInfo>Age</EachGuestInfo>
-        <EachGuestInfo>Gender</EachGuestInfo>
-        <EachGuestInfo>Amount Due</EachGuestInfo>
+          <EachGuestInfo>Name</EachGuestInfo>
+          <EachGuestInfo>RSVP</EachGuestInfo>
+          <EachGuestInfo>Age</EachGuestInfo>
+          <EachGuestInfo>Gender</EachGuestInfo>
+          <EachGuestInfo>Amount Due</EachGuestInfo>
         </EachGuest>
 
-      {guests?.map((guest) => (
+        {guests?.map((guest) => (
           <EachGuest key={guest.lastName + guest.firstName}>
-            <EachGuestInfo> {guest.firstName} {guest.lastName}</EachGuestInfo>
+            <EachGuestInfo>
+              {guest.firstName} {guest.lastName}
+            </EachGuestInfo>
             <EachGuestInfo>{guest.RSVP}</EachGuestInfo>
             <EachGuestInfo>{guest.age} </EachGuestInfo>
             <EachGuestInfo>{guest.gender}</EachGuestInfo>
             <EachGuestInfo>${guest.amountDue}</EachGuestInfo>
-            
-        </EachGuest>
-      ))}
-              </TableWrapper>
-
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="firstName">
-          First Name:
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            value={newGuest.firstName}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="lastName">
-          Last Name:
-          <input
-            type="text"
-            id="firstName"
-            name="lastName"
-            value={newGuest.lastName}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="gender">Gender:</label>
-        <select id="gender" name="gender" onChange={handleChange}>
-          <option value="female">Female</option>
-          <option value="male">Male</option>
-        </select>
-        <label htmlFor="RSVP">RSVP:</label>
-        <select id="RSVP" name="RSVP" onChange={handleChange}>
-          <option value="pending">Pending</option>
-          <option value="yes">Yes</option>
-          <option value="no">No</option>
-          <option value="maybe">Maybe</option>
-        </select>
-        <label htmlFor="age">
-          Age:
-          <input
-            type="number"
-            id="age"
-            name="age"
-            value={newGuest.age}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="amountDue">
-          Amount Due:
-          <input
-            type="number"
-            id="amountDue"
-            name="amountDue"
-            value={newGuest.amountDue}
-            onChange={handleChange}
-          />
-        </label>
-        <input type="submit" />
-      </form>
+          </EachGuest>
+        ))}
+      </TableWrapper>
     </div>
   );
 }
